5d6144068ba3a09fcf66a7df396548331fa86504
ChieveiT <javachieveit@163.com>
2016-02-14 23:44:40 +0800

    clear

--- Protocol/Http/NotFoundException.php
+++
@@ -1,8 +0,0 @@
-<?php namespace ChieveiT\WTF\Protocol\Http;
-
-use Exception;
-
-class NotFoundException extends Exception
-{
-    
-}
--- Services/Provider.php
+++
@@ -1,6 +0,0 @@
-<?php namespace ChieveiT\WTF\Services;
-
-class Provider
-{
-    
-}
