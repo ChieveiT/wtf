43f0f43bd3643dfe8294e359d85c012f830e0f18
ChieveiT <javachieveit@163.com>
2016-02-14 23:45:48 +0800

    add vendor

---
+++ Http/Request.php
@@ -0,0 +1,24 @@
+<?php namespace ChieveiT\WTF\Http;
+
+class Request
+{
+    private $header;
+    private $body;
+    
+    private $args = [];
+    
+    public function header()
+    {
+    
+    }
+    
+    public function body()
+    {
+    
+    }
+    
+    public function args()
+    {
+    
+    }
+}
---
+++ Http/Response.php
@@ -0,0 +1,6 @@
+<?php namespace ChieveiT\WTF\Http;
+
+class Response
+{
+    
+}
